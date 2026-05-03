6368bf96611c9c9f97a16bc5cda428b5c1b8c8c4
Exerted754 <ragalik754.lug@gmail.com>
2026-05-03 23:05:29 +0300

    получение транзацкций с фильтром

--- src/Repository/TransactionRepository.php
+++ src/Repository/TransactionRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Transaction;
+use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -11,5 +12,35 @@ class TransactionRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Transaction::class);
+    }
+
+    public function findByUserWithFilters(User $user, array $filters): array
+    {
+        $qb = $this->createQueryBuilder('t')
+            ->leftJoin('t.course', 'c')
+            ->addSelect('c')
+            ->andWhere('t.user = :user')
+            ->setParameter('user', $user)
+            ->orderBy('t.createdAt', 'DESC');
+
+        if (!empty($filters['type'])) {
+            $qb
+                ->andWhere('t.type = :type')
+                ->setParameter('type', $filters['type']);
+        }
+
+        if (!empty($filters['course_code'])) {
+            $qb
+                ->andWhere('c.code = :courseCode')
+                ->setParameter('courseCode', $filters['course_code']);
+        }
+
+        if (!empty($filters['skip_expired'])) {
+            $qb
+                ->andWhere('t.expiresAt IS NULL OR t.expiresAt > :now')
+                ->setParameter('now', new \DateTimeImmutable());
+        }
+
+        return $qb->getQuery()->getResult();
     }
 }
