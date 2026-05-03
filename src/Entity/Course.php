49e84a801f0f89e3502305bb87961027c9c64728
Exerted754 <ragalik754.lug@gmail.com>
2026-05-03 19:22:58 +0300

    added Course Model

---
+++ src/Entity/Course.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CourseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CourseRepository::class)]
+#[ORM\Table(name: 'billing_course')]
+class Course
+{
+    public const TYPE_FREE = 'free';
+    public const TYPE_BUY = 'buy';
+    public const TYPE_RENT = 'rent';
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, unique: true)]
+    private ?string $code = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $type = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): static
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
